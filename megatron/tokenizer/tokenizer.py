7e4c8b83435172ac4ae9a3d1f9bbdd1ea8513e08
HuangLK <hunaglk3@gmail.com>
2023-07-14 11:12:19 +0800

    add minor modifications

--- megatron/tokenizer/tokenizer.py
+++ megatron/tokenizer/tokenizer.py
@@ -190,7 +190,7 @@ class SentencePieceTokenizer(AbstractTokenizer):
         super().__init__(name)
 
         self.tokenizer = spm.SentencePieceProcessor(model_file=vocab_file)
-        self.eod_id = self.tokenizer.piece_to_id("<|endoftext|>")
+        self.eod_id = self.tokenizer.piece_to_id("</s>")
 
     @property
     def vocab_size(self):
